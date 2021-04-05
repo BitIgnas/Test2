75d56a89b7d2af712c1b25a3ad1d6c485ffe9ffd
IgnatLT79 <71728683+IgnatLT79@users.noreply.github.com>
2021-04-05 13:25:50 +0300

    added comments and sms

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,14 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("some text");
+        //
+        //
+        //
+        //
+        //
+        //
+        //
+        //
     }
 }

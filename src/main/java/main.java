2f79f62606ce4bccc609648dc02f3023342ce6b5
IgnatLT79 <71728683+IgnatLT79@users.noreply.github.com>
2021-04-05 13:28:09 +0300

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,13 +2,9 @@ public class main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("some text");
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
+        System.out.println("no");
+
+        
+  
     }
 }
